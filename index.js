7011eac45790c155ebfec3ce47c5110ea4b7ca5e
sidmaskey13 <sid.mas13@gmail.com>
2021-05-25 18:53:34 +0545

    basic paypal integration done

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-
+require('dotenv').config()
 const express = require('express')
+var cors = require('cors')
 const paypal = require('paypal-rest-sdk');
 const app = express()
 const port = 4000
+app.use(cors())
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
-    'client_id': '####yourclientid######',
-    'client_secret': '####yourclientsecret#####'
+    'client_id': 'Ae-IBehcksg_uUUtmf90x_R7oKJxxQHwsiE75t_x48ZFoKzIyJoCuVBD8-QJFj7RRZ7QTFsgIQjED2cl',
+    'client_secret': `${process.env.PAYPAL_SECRET_KEY}`
 });
 
 
@@ -15,10 +17,85 @@ app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
 
 app.get("/", function (req, res) {
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+    var tagline = "Working on paypal integration";
     res.render("table.ejs", { tagline: tagline });
 });
 
+app.post("/pay", function (req, res) {
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"
+        },
+        "redirect_urls": {
+            "return_url": "http://localhost:4000/success",
+            "cancel_url": "http://localhost:4000/cancel"
+        },
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "Mango",
+                    "sku": "mango",
+                    "price": "30",
+                    "currency": "USD",
+                    "quantity": 1
+                },
+                {
+                    "name": "Banana",
+                    "sku": "banana",
+                    "price": "15",
+                    "currency": "USD",
+                    "quantity": 1
+                }]
+            },
+            "amount": {
+                "currency": "USD",
+                "total": "45"
+            },
+            "description": "Fruits"
+        }]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            for (let i = 0; i < payment.links.length; i++) {
+                if (payment.links[i].rel === 'approval_url') {
+                    res.redirect(payment.links[i].href);
+                }
+            }
+        }
+    });
+});
+
+//validate
+app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+
+    const execute_payment_json = {
+        "payer_id": payerId,
+        "transactions": [{
+            "amount": {
+                "currency": "USD",
+                "total": "45"
+            }
+        }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log(JSON.stringify(payment));
+            res.send('Success');
+        }
+    });
+});
+
+app.get('/cancel', (req, res) => res.send('Cancelled'));
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`)
